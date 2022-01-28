c3cb5d553326eaae7243b4f845035c3e04355011
Anastasiia Duplishcheva <a.duplishcheva@gmail.com>
2022-01-28 15:22:29 +0300

    change structure project

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import { defaultCardButton, horizontalCardButton } from '../utils/constants.js';
+import renderElements from '../utils/utils.js';
+
+defaultCardButton.addEventListener('click', () => {
+  renderElements(true);
+});
+
+horizontalCardButton.addEventListener('click', () => {
+  renderElements(false);
+});
+
+renderElements();
