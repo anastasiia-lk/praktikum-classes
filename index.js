ec959e7f2b5ab2c6e095486dda9bc64e0a2788ba
Anastasiia Duplishcheva <a.duplishcheva@gmail.com>
2022-01-28 13:35:44 +0300

    add popup cards

--- index.js
+++ index.js
@@ -19,16 +19,16 @@ const items = [
   },
 ];
 
+const cardList = document.querySelector('.card-list__items');
 const popupElement = document.querySelector('.popup');
-const popupImage = document.querySelector('.popup__image');
 const popupCloseButton = document.querySelector('.popup__close');
+const popupImage = document.querySelector('.popup__image');
+const popupCaption = document.querySelector('.popup__caption');
+const defaultCardButton = document.querySelector('.filter__button_type_grid');
+const horizontalCardButton = document.querySelector('.filter__button_type_column');
 
 class Card {
-  constructor(data, selector) {
-    this._title = data.title;
-    this._description = data.description;
-    this._price = data.price;
-    this._image = data.image;
+  constructor(selector) {
     this._selector = selector;
   }
 
@@ -42,43 +42,96 @@ class Card {
     return cardElement;
   }
 
+  _handleOpenPopup() {
+    popupImage.src = this._image;
+    popupElement.classList.add('popup_is-opened');
+  }
+
+  _handleClosePopup() {
+    popupImage.src = '';
+    popupElement.classList.remove('popup_is-opened');
+  }
+
+  _setEventListeners() {
+    this._element.addEventListener('click', () => {
+      this._handleOpenPopup();
+    });
+
+    popupCloseButton.addEventListener('click', () => {
+      this._handleClosePopup();
+    });
+  }
+}
+
+class DefaultCard extends Card {
+  constructor(data, selector) {
+    super(selector);
+    this._title = data.title;
+    this._description = data.description;
+    this._image = data.image;
+  }
+
   generate() {
-    this._element = this._getElement();
+    this._element = super._getElement();
+    super._setEventListeners();
 
     this._element.querySelector('.card__image').style.backgroundImage = `url(${this._image})`;
     this._element.querySelector('.card__title').textContent = this._title;
-    this._element.querySelector('.card__info').textContent = this._description;
-    this._element.querySelector('.card__price-property').textContent = this._price;
-    
-    this._setEventListeners();
 
     return this._element;
   }
   
   _handleOpenPopup() {
-    popupImage.src = this._image;
-    popupElement.classList.add('popup_is-opened');
+    popupCaption.textContent = this._description;
+    super._handleOpenPopup();
   }
   
-    _handleClosePopup() {
-    popupImage.src = '';
-    popupElement.classList.remove('popup_is-opened');
-  }
-  
-  _setEventListeners() {
-    this._element.addEventListener('click', () => {
-      this._handleOpenPopup();
-    })
-        popupCloseButton.addEventListener('click', () => {
-      this._handleClosePopup();
-    })
+  _handleClosePopup() {
+    popupCaption.textContent = '';
+    super._handleClosePopup();
   }
 }
 
-items.forEach((item) => {
-  const card = new Card(item, '.horizontal-card');
-  const cardElement = card.generate();
+class HorizontalCard extends Card {
+  constructor(data, selector) {
+    super(selector);
+    this._title = data.title;
+    this._description = data.description;
+    this._price = data.price;
+    this._image = data.image;
+  }
 
-  // Добавляем в DOM
-  document.querySelector('.card-list__items').append(cardElement);
+  generate() {
+    this._element = super._getElement();
+    super._setEventListeners();
+
+    this._element.querySelector('.card__image').style.backgroundImage = `url(${this._image})`;
+    this._element.querySelector('.card__title').textContent = this._title;
+    this._element.querySelector('.card__info').textContent = this._description;
+    this._element.querySelector('.card__price-property').textContent = this._price;
+
+    return this._element;
+  }
+}
+
+const renderElements = (isGrid) => {
+  cardList.innerHTML = '';
+  items.forEach((item) => {
+    const card = isGrid
+      ? new DefaultCard(item, '.default-card')
+      : new HorizontalCard(item, '.horizontal-card');
+
+    const cardElement = card.generate();
+    cardList.append(cardElement);
+  });
+};
+
+defaultCardButton.addEventListener('click', () => {
+  renderElements(true);
 });
+
+horizontalCardButton.addEventListener('click', () => {
+  renderElements(false);
+});
+
+renderElements();
